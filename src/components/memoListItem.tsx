463340d7a853bd1116df84be3e73cb0f3412d0a3
mavinii <m.oliveirad02@gmail.com>
2024-08-20 20:23:36 +0100

    feat: :construction: Audio is working
    It is possible now to play the sound and it displays the time-stemp

--- src/components/memoListItem.tsx
+++ src/components/memoListItem.tsx
@@ -1,28 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { Play } from 'lucide-react-native';
-import { View, Button, TouchableOpacity } from 'react-native';
-import { Audio } from 'expo-av';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Pause, Play } from 'lucide-react-native';
+import { View, Button, TouchableOpacity, Text } from 'react-native';
+import { Audio, AVPlaybackStatus } from 'expo-av';
 import { Sound } from 'expo-av/build/Audio';
 
 const MemosListItem = ({ uri }: { uri: string }) => {
     const [sound, setSound] = useState<Sound>();
-
-    async function loadSoung() {
+    const [status, setStatus] = useState<AVPlaybackStatus>()
+    
+    async function loadSound() {
         console.log('Loading Sound');
-        const { sound } = await Audio.Sound.createAsync({ uri });
+        const { sound } = await Audio.Sound.createAsync({ uri }, undefined, onPlayBackStatusUpdate);
         setSound(sound);
     }
 
-    useEffect(() =>{
-        loadSoung();
+    // Effect to load sound when URI changes
+    useEffect(() => {
+        loadSound();
+        return () => {
+        // Cleanup the sound on component unmount
+        sound?.unloadAsync();
+        };
     }, [uri]);
+
+    const onPlayBackStatusUpdate = useCallback (
+        async (newStatus: AVPlaybackStatus) => {
+            console.log('Playback Status Updated');
+            setStatus(newStatus);
+            
+            if (!newStatus.isLoaded || !sound) {
+                console.log('Sound is not loaded');
+                return;
+            }
+            
+            if (newStatus.didJustFinish) {
+                await sound?.setPositionAsync(0);
+            }
+        }, 
+        [sound]
+    );
 
     async function playSound() {
         if(!sound){
             return;
         }
       console.log('Playing Sound');
-      await sound.playAsync();
+      if (status?.isLoaded && status.isPlaying) {
+        console.log('Pausing Sound');
+        await sound.pauseAsync();
+        return;
+      } else {
+        await sound.replayAsync();
+        return;
+        }
     }
   
     useEffect(() => {
@@ -34,56 +64,44 @@ const MemosListItem = ({ uri }: { uri: string }) => {
         : undefined;
     }, [sound]);
 
+    const isPlaying = status?.isLoaded ? status.isPlaying : false;
+    const position = status?.isLoaded ? status.positionMillis : 0;
+    const duration = status?.isLoaded ? status.durationMillis : 1;
+    const progress = duration ? position / duration : 0;
 
-
-
-
-    // // Initialize progress state
-    // const [progress, setProgress] = useState(0);
-    // const [isPlaying, setIsPlaying] = useState(false);
-
-    // // Function to handle progress update
-    // const playBackIndicator = () => {
-    //     setIsPlaying(true); // Start updating progress
-    // };
-
-    // // useEffect to increment progress every second when playing
-    // useEffect(() => {
-    //     let interval;
+    // converts milliseconds to hours, minutes, and seconds
+    const convertToTime = (ms: number) => {
+        const hours = Math.floor(ms / 3600000);
+        const minutes = Math.floor((ms % 3600000) / 60000);
+        const seconds = Math.floor(((ms % 360000) % 60000) / 1000);
         
-    //     if (isPlaying && progress < 100) {
-    //         interval = setInterval(() => {
-    //             setProgress(prev => {
-    //             const newProgress = prev + 1;
-    //             if (newProgress >= 100) {
-    //                 clearInterval(interval); // Stop when reaching 100%
-    //                 setIsPlaying(false);
-    //             }
-    //             return newProgress;
-    //             });
-    //         }, 1000); // Update progress every second
-    //     }
-    //     return () => clearInterval(interval); // Cleanup interval on component unmount
-    // }, [isPlaying, progress]);
+        return `${hours ? hours + ':' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
 
-    
     return (
         <View className='h-20 bg-zinc-200 px-4 rounded-xl flex flex-row items-center gap-3 border border-zinc-50'>
             <TouchableOpacity
                 className='fixed inset-0 items-center gap-2'
                 onPress={playSound}
                 >
-                <Play className='bg-zinc-100' size={23} />
+                    {isPlaying ? (
+                        <Pause className='bg-zinc-100' size={23} />
+                    ) : (
+                        <Play className='bg-zinc-100' size={23} />
+                    )}
             </TouchableOpacity>
     
             <View className='flex-1'>
                 <View className='flex flex-row h-1 bg-neutral-300 rounded-sm items-center'>
                     <View
-                    className='w-3 aspect-square bg-[royalblue] rounded-full absolute'
-                    // style={{ left: `${progress}%` }} // Dynamically update the left position
+                        className='w-3 aspect-square bg-[royalblue] rounded-full absolute'
+                        style={{ left: `${progress * 100}%` }} // Dynamically update the left position
                     />
                 </View>
             </View>
+            <Text className='ml-1 color-neutral-500'>
+                {convertToTime(position ?? 0)} / {convertToTime(duration ?? 0)}
+            </Text>
         </View>
     ) 
 }
